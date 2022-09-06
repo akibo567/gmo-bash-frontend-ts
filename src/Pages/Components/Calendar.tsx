4b477518a3c95c5e6ee7aeb3e871947d29cf1a55
ponzukun <p1o1n1z1u@gmail.com>
2022-09-06 17:55:14 +0900

    模擬データを型定義に合わせた

--- src/Pages/Components/Calendar.tsx
+++ src/Pages/Components/Calendar.tsx
@@ -1,80 +1,94 @@
 import FullCalendar, { DateSelectArg } from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { EventList } from '../../Types/EventList';
+import { Event } from '../../Types/Event';
 
 
 const Calendar = () => {
-  let menuList: any = [];
-
-  menuList.push(
-    {
-      title: '牛丼',
-      date: '2022-09-22',
-      color: 'red',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: '卵',
-      date: '2022-09-22',
-      color: 'black',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: 'シェフの気まぐれサラダ',
-      date: '2022-09-22',
-      color: 'blue',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: '牛丼',
-      date: '2022-09-23',
-      color: 'red',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: '卵',
-      date: '2022-09-23',
-      color: 'black',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: 'シェフの気まぐれサラダ',
-      date: '2022-09-23',
-      color: 'blue',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: '牛丼',
-      date: '2022-09-24',
-      color: 'red',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: '卵',
-      date: '2022-09-24',
-      color: 'black',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-    {
-      title: 'シェフの気まぐれサラダ',
-      date: '2022-09-24',
-      color: 'blue',
-      money: '100',
-      material: 'ブロッコリー, 白菜'
-    },
-  )
+  const eventList: EventList = {
+    result: [
+      {
+        title: "牛丼",
+        date: "2022-09-22",
+        color: "red",
+        recipe: {
+          recipeId: 1,
+          recipeName: '牛丼',
+          recipeImage: "image",
+          recipeUrl: "url",
+          recipeCost: "100",
+          recipeIndication: "5分",
+          recipeCategory: "昼",
+          recipeMaterials: ["ブロッコリー", "白菜"],  
+        },
+      },
+      {
+        title: "牛丼",
+        date: "2022-09-22",
+        color: "blue",
+        recipe: {
+          recipeId: 1,
+          recipeName: '牛丼',
+          recipeImage: "image",
+          recipeUrl: "url",
+          recipeCost: "100",
+          recipeIndication: "5分",
+          recipeCategory: "昼",
+          recipeMaterials: ["ブロッコリー", "白菜"],  
+        },
+      },
+      {
+        title: "牛丼",
+        date: "2022-09-23",
+        color: "red",
+        recipe: {
+          recipeId: 1,
+          recipeName: '牛丼',
+          recipeImage: "image",
+          recipeUrl: "url",
+          recipeCost: "100",
+          recipeIndication: "5分",
+          recipeCategory: "昼",
+          recipeMaterials: ["ブロッコリー", "白菜"],  
+        },
+      },
+      {
+        title: "牛丼",
+        date: "2022-09-23",
+        color: "green",
+        recipe: {
+          recipeId: 1,
+          recipeName: '牛丼',
+          recipeImage: "image",
+          recipeUrl: "url",
+          recipeCost: "100",
+          recipeIndication: "5分",
+          recipeCategory: "昼",
+          recipeMaterials: ["ブロッコリー", "白菜"],  
+        },
+      },
+      {
+        title: "牛丼",
+        date: "2022-09-24",
+        color: "red",
+        recipe: {
+          recipeId: 1,
+          recipeName: '牛丼',
+          recipeImage: "image",
+          recipeUrl: "url",
+          recipeCost: "100",
+          recipeIndication: "5分",
+          recipeCategory: "昼",
+          recipeMaterials: ["ブロッコリー", "白菜"],  
+        },
+      },
+    ]
+  };
 
   const handleDateSelect = (selectionInfo: DateSelectArg) => {
     const selectDate: string = selectionInfo.startStr;
-    const result = menuList.filter((menu: any) => menu.date === selectDate);
+    const result = eventList.result.filter((event: Event) => event.date === selectDate);
     console.log(result);
 }
 
@@ -89,7 +103,7 @@ const Calendar = () => {
         right: 'next', 
       }}
       initialView="dayGridMonth" // 初期表示のモードを設定する
-      events={menuList}
+      events={eventList.result}
       contentHeight='auto'
       selectable={true}
       select={handleDateSelect}
