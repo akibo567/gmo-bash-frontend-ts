5e7e352565359898a5ff297d2f15b5fd0e31d670
akibo567 <akibo567@live.jp>
2022-09-07 18:33:09 +0900

    ログイン画面つくりかけ

---
+++ src/Pages/LoginPage.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import TextField from '@mui/material/TextField';
+
+
+import MenuCard from './Components/MenuCard';
+import {Recipe} from '../Types/Recipe';
+import {RootState} from '../store';
+import {API_ENDPOINT,
+   BREAKFAST_TIME, 
+   LUNCH_TIME, 
+   DINNER_TIME,
+   BREAKFAST_MENU_TYPES,
+   LUNCH_MENU_TYPES,
+   DINNER_MENU_TYPES,
+} from '../Setting';
+
+
+
+const MenuPage = () => {
+  const navigate = useNavigate();
+
+  
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div style={style_LoginPage}>
+      <>
+      <TextField id="filled-basic" label="userID" variant="filled" />
+      <TextField id="filled-basic" label="password" variant="filled" />
+      </>
+    </div>
+  );
+}
+
+const style_TabPanel = {
+  background: '#dcdcdc'
+};
+
+const style_MenuListContainer: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+};
+
+
+const style_LoginPage = {
+  flex: 1,
+  //background: '#dcdcdc'
+};
+
+export default MenuPage
